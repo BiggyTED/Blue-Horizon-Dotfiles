8009617787c35dc88dc2b6547bab642da185f183
Caleb Carter <70119123+BiggyTED@users.noreply.github.com>
2021-12-20 14:24:31 -0600

    Updated Configs
    Changed applications, Rofi config, added multi-monitor support

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,12 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar topBar 2>&1 | tee -a /tmp/polybar1.log & disown
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload topBar &
+done
+
+#echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+#polybar topBar 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Polybar launched"
